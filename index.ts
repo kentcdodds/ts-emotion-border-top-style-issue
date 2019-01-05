f9c07292267ae2d21628aaf433e835053459921f
Karol Majewski <20233319+karol-majewski@users.noreply.github.com>
2019-01-05 03:45:20 +0100

    Show where the problem is (#1)

--- index.ts
+++ index.ts
@@ -1,12 +1,37 @@
-import styled from '@emotion/styled'
+import styled, { Interpolation, CSSObject } from '@emotion/styled'
 
 const WorkingDivider = styled.hr({borderTopStyle: 'solid'})
 
+/**
+ * In object literals, only the keys are treated literally.
+ * In this case, the value will be widened to type `string`.
+ * Because of that, `baseStyles` does not match the first overload
+ * defined by Emotion for `styled.hr` and TypeScript will think
+ * your intention was to provide a template string as argument.
+ */
 const baseStyles = {borderTopStyle: 'solid'}
-const NotWorkingDivider = styled.hr(baseStyles)
+styled.hr(baseStyles)
 
-const otherBaseStyles = {margin: 20}
-const AnotherWorkingDivider = styled.hr(otherBaseStyles)
+/**
+ * Telling TypeScript to treat the object literally would help.
+ */
+const literal: { borderTopStyle: 'solid' } = {borderTopStyle: 'solid'};
+styled.hr(literal);
 
-export {WorkingDivider, NotWorkingDivider, AnotherWorkingDivider}
+/**
+ * This will also work because when a type definition is used,
+ * TypeScript will try to match the provided object by treating it literally.
+ */
+const cssObject: CSSObject = {borderTopStyle: 'solid'};
+styled.hr(cssObject);
 
+/**
+ * Or you can just pick a wider type that will still match the first overload.
+ */
+const interpolation: Interpolation = {borderTopStyle: 'solid'};
+styled.hr(interpolation);
+
+/**
+ * Note: the following will not work! `baseStyles` is still just a `{ borderTopStyle: string }`
+ */
+const nope: Interpolation = baseStyles;
